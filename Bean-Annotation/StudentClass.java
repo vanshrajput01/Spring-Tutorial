96edb78d19227263d71a5617d1f33cbb0b8daf96
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-09 13:47:51 +0530

    Create StudentClass.java

---
+++ Bean-Annotation/StudentClass.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+
+
+public class Student {
+	
+	@Value("#{101}")
+	public Integer studId;
+	@Value("name02")
+	public String studName;
+	
+
+
+
+
+	
+	
+
+}
