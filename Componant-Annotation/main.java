d3f7f1d01cbdabe0edd61e8252b7c228abd23b0c
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-09 12:49:50 +0530

    Main class

---
+++ Componant-Annotation/main.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// in your main class Method have @SpringBootApplication so you can not add @component-scan(),@configuration,@EnableAutoConfiguration these Annotation..
+public class Program01Application implements CommandLineRunner {
+	@Autowired
+	private Student student;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program01Application.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.print(student.studName);
+		System.out.println(student.studId);
+
+	}
+
+}
